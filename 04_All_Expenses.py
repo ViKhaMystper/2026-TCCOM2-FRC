28279c98e308b9ce556323fb954745394bfd7af3
ViKhaMystper <tranb4547@masseyhigh.school.nz>
2026-04-02 10:24:59 +1300

    Total expenses calculation

---
+++ 04_All_Expenses.py
@@ -0,0 +1,165 @@
+import pandas
+from tabulate import tabulate
+
+def not_blank(question):
+    """Check that a user response is not blank"""
+
+    while True:
+        response = input(question)
+
+        if response != "":
+            return response
+
+        print("Sorry, this can't be blank. Please try again. \n")
+
+def num_check(question, num_type="float", exit_code = None):
+    """Check users enter an integer / float that is more than zero
+    (or the optional exit code)"""
+
+    if num_type == "float":
+        error = "Please enter an number more than 0."
+    else:
+        error = "Please enter an integer more than 0."
+
+    while True:
+
+        response = input(question)
+
+        # check for the exit code and return it if entered
+        if response == exit_code:
+            return response
+
+
+        # Check datatype is correct and that number is more than 0
+        try:
+            if num_type == "float":
+                response = float(response)
+            else:
+                response = int(response)
+
+
+            if response > 0:
+                return response
+            else:
+                print(error)
+
+        except ValueError:
+            print(error)
+
+def get_expenses(exp_type, how_many=1):
+    """Gets a variable / fix expenses and outputs pandas (as a string) and a
+    subtotal of the expenses."""
+
+    # List for panda
+    all_item = []
+    all_amount = []
+    all_dollar_per_item = []
+
+    # Expenses dictionary
+    expenses_dict = {
+        "Item": all_item,
+        "Amount": all_amount,
+        "$ / Item": all_dollar_per_item
+    }
+
+    # Defaults for fixed expenses
+    amount = how_many # how many defaults to 1
+    how_much_question = "How much? $"
+
+    # loop to get expenses
+    while True:
+        # Get item name and check it's not blank
+        item_name = not_blank("Item name: ")
+
+        # Checks users enter at least one variable expenses
+        if exp_type == "variable" and item_name == "xxx" and len(all_item) == 0:
+            print("Oops - You have not entered anything. "
+                  "You need at least one item.")
+            continue
+
+        # End loop when users enter exit code
+        elif item_name == "xxx":
+            break
+
+        # Get variable expenses item amount <enter> default to number of products being made
+        if exp_type == "variable":
+
+
+            amount = num_check(f"How many <enter for {how_many}>: ",
+                           "integer","")
+
+            # Allow users to push <enter> to default to number of items being made
+            if amount == "":
+                amount = how_many
+
+            how_much_question = "Price for one? $"
+
+
+        # Get price for item (question customized depending on expense type)
+        price_for_one = num_check(how_much_question, "float")
+
+        all_item.append(item_name)
+        all_amount.append(amount)
+        all_dollar_per_item.append(price_for_one)
+
+    # Make panda
+    expense_frame = pandas.DataFrame(expenses_dict)
+
+    # Calculate Row Cost
+    expense_frame['Cost'] = expense_frame['Amount'] * expense_frame['$ / Item']
+
+    # Calculate subtotal
+    subtotal = expense_frame['Cost'].sum()
+
+    # Apply currency formatting to currency columns
+    add_dollars = ['Amount', '$ / Item', 'Cost']
+    for var_item in add_dollars:
+        expense_frame[var_item] = expense_frame[var_item].apply(currency)
+
+    # Make expense frame into a string with the desired columns
+    if exp_type == "Variable":
+        expense_string = tabulate(expense_frame, headers='keys',
+                                  tablefmt='psql', showindex=False)
+
+    else:
+        expense_string = tabulate(expense_frame[['Item', 'Cost']],
+                                  headers='keys', tablefmt='psql', showindex=False)
+
+    # return the expenses panda and subtotal
+    return expense_string, subtotal
+
+def currency(x):
+    return "${:.2f}".format(x)
+
+
+# Main routine starts here
+
+quantity_made = num_check("Quantity being made: ", "integer")
+print()
+
+print("Getting Variable Costs")
+variable_expenses = get_expenses("variable", quantity_made)
+print()
+variable_panda = variable_expenses [0]
+variable_subtotal = variable_expenses [1]
+
+print("Getting Fixed Costs...")
+fixed_expenses = get_expenses("fixed")
+print()
+fixed_panda = fixed_expenses [0]
+fixed_subtotal = fixed_expenses [1]
+
+# Temporary output area (for easy testing)
+
+print("=== Variable Expenses ===")
+print(variable_panda)
+print(f"Variable subtotal: ${variable_subtotal:.2f}")
+print()
+
+print("=== Fixed Expenses ===")
+print(fixed_panda)
+print(f"Fixed Subtotal: ${fixed_subtotal:.2f}")
+
+print()
+total_expenses = variable_subtotal + fixed_subtotal
+print(f"Total Expenses: ${total_expenses:.2f}")
